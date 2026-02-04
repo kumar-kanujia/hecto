bb708f4b25501a8a8a0942e0278dd81c69447646
Kumar <70059467+kumar-kanujia@users.noreply.github.com>
2026-02-04 15:57:14 +0530

    update: added added attribute mapping

---
+++ src/editor/terminal/attribute.rs
@@ -0,0 +1,40 @@
+use crossterm::style::Color;
+
+use crate::editor::annotatedstring::annotationtype::AnnotationType;
+
+/// Defines an attribute which the terminal can use
+pub struct Attribute {
+  pub foreground: Option<Color>,
+  pub background: Option<Color>,
+}
+
+impl From<AnnotationType> for Attribute {
+  fn from(annotation_type: AnnotationType) -> Self {
+    match annotation_type {
+      AnnotationType::Match => Self {
+        foreground: Some(Color::Rgb {
+          r: 255,
+          g: 255,
+          b: 255,
+        }),
+        background: Some(Color::Rgb {
+          r: 100,
+          g: 100,
+          b: 100,
+        }),
+      },
+      AnnotationType::SelectMatch => Self {
+        foreground: Some(Color::Rgb {
+          r: 255,
+          g: 255,
+          b: 255,
+        }),
+        background: Some(Color::Rgb {
+          r: 255,
+          g: 251,
+          b: 0,
+        }),
+      },
+    }
+  }
+}
