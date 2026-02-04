2c9a7699e7899ba18382e82e66ebed2c7e0176ee
Kumar <70059467+kumar-kanujia@users.noreply.github.com>
2026-02-04 15:04:10 +0530

    refactor: commands

---
+++ src/editor/command/mod.rs
@@ -0,0 +1,42 @@
+pub mod edit;
+pub mod movecommand;
+pub mod system;
+
+pub use crate::editor::{
+  command::{edit::Edit, movecommand::Move, system::System},
+  size::Size,
+};
+
+use crossterm::event::Event;
+
+#[derive(Clone, Copy)]
+pub enum Command {
+  Move(Move),
+  Edit(Edit),
+  System(System),
+}
+
+// clippy::as_conversions: Will run into problems for rare edge case systems where usize < u16
+#[allow(clippy::as_conversions)]
+impl TryFrom<Event> for Command {
+  type Error = String;
+
+  fn try_from(event: Event) -> Result<Self, Self::Error> {
+    match event {
+      Event::Key(key_event) => Edit::try_from(key_event)
+        // Try to convert key_event into an Edit event
+        .map(Command::Edit)
+        // If fail, try to convert key_event into a Move event
+        .or_else(|_| Move::try_from(key_event).map(Command::Move))
+        // If fail, try to convert key_event into a System event
+        .or_else(|_| System::try_from(key_event).map(Command::System))
+        // If fail, return an error
+        .map_err(|_err| format!("Event not supported: {key_event:?}")),
+      Event::Resize(width_u16, height_u16) => Ok(Self::System(System::Resize(Size {
+        height: height_u16 as usize,
+        width: width_u16 as usize,
+      }))),
+      _ => Err(format!("Event not supported: {event:?}")),
+    }
+  }
+}
---
+++ src/editor/command/system.rs
@@ -0,0 +1,39 @@
+use crate::editor::size::Size;
+
+use crossterm::event::{
+  KeyCode::{Char, Esc},
+  KeyEvent, KeyModifiers,
+};
+
+#[derive(Clone, Copy)]
+pub enum System {
+  Save,
+  Resize(Size),
+  Quit,
+  Dismiss,
+  Search,
+}
+
+impl TryFrom<KeyEvent> for System {
+  type Error = String;
+  fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
+    let KeyEvent {
+      code, modifiers, ..
+    } = event;
+
+    if modifiers == KeyModifiers::CONTROL {
+      match code {
+        Char('q') => Ok(Self::Quit),
+        Char('s') => Ok(Self::Save),
+        Char('f') => Ok(Self::Search),
+        _ => Err(format!("Unsupported CONTROL+{code:?} combination")),
+      }
+    } else if modifiers == KeyModifiers::NONE && matches!(code, Esc) {
+      Ok(Self::Dismiss)
+    } else {
+      Err(format!(
+        "Unsupported key code {code:?} or modifier {modifiers:?}"
+      ))
+    }
+  }
+}
